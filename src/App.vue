a421a835d4fda30a08fafc4c72251d34983a555b
Raúl Pérez Portolés <raul.perez.portoles@gmail.com>
2025-09-16 00:32:07 +0200

    Changes in App.vue and vite config

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="d-flex vh-100">
+    <Navbar />
+    <main class="flex-grow-1 p-4">
+      <router-view />
+    </main>
+  </div>
 </template>
+
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+</script>
 
 <style scoped></style>
