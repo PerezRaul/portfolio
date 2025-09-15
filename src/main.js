978c51a2007f58d7ac0519bdf176fc65bbac57a4
Raúl Pérez Portolés <raul.perez.portoles@gmail.com>
2025-09-16 00:31:39 +0200

    Add router, translation and bootstrap to main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import i18n from './locales'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+createApp(App).use(router).use(i18n).mount('#app')
