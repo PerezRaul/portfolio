95a9d2333677c2381c7fd8023f04da3facce00e8
Raúl Pérez Portolés <raul.perez.portoles@gmail.com>
2025-09-20 20:14:25 +0200

    Prepare project for github pages

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  // base: '/portfolio/',
+  base: '/portfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
